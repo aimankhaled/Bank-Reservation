b30a6760e092177afcaa9491bf51cd59ebd18d3a
aiman khaled <aimankhaled20@gmail.com>
2021-04-20 19:53:01 +0200

    image picker

--- app/src/main/java/com/example/ehgezli/EditProfile.java
+++ app/src/main/java/com/example/ehgezli/EditProfile.java
@@ -1,16 +1,32 @@
 package com.example.ehgezli;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.media.MediaActionSound;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.FileUtils;
+import android.provider.MediaStore;
+import android.provider.OpenableColumns;
 import android.util.Patterns;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,14 +42,26 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+
 public class EditProfile extends AppCompatActivity {
 
     private FirebaseUser user;
     private DatabaseReference reference;
     private  String userId;
     private EditText mobileEditText,fullNameEditText,ageEditText,addressEditText;
-    private Button updateProfileBtn;
+    private Button updateProfileBtn,editProfilePic_Btn;
     private ProgressBar progressBar;
+    private ImageView profilePic_Image;
+
+    private static final int image_pick_code = 1000;
+    private static final int permission_code = 1001;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -139,6 +167,26 @@ public class EditProfile extends AppCompatActivity {
             }
         });
 
+        profilePic_Image = (ImageView) findViewById(R.id.profilePic_Image);
+        editProfilePic_Btn=(Button)findViewById(R.id.editProfilePic_Btn);
+        editProfilePic_Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                        //permission not granted ask for it
+                        String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+                        requestPermissions(permissions, permission_code);
+                    } else {
+                        //permission granted
+                        pickImageFromGallery();
+                    }
+                } else {
+                    //system os less than marshmallow
+                    pickImageFromGallery();
+                }
+            }
+        });
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -149,4 +197,54 @@ public class EditProfile extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+    private void pickImageFromGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, image_pick_code);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode)
+        {
+            case permission_code:{
+                if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED)
+                {
+                    pickImageFromGallery();
+                }
+                else
+                {
+                    Toast.makeText(this,"Permission denied... !",Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (-1 == RESULT_OK && requestCode == image_pick_code) {
+            Uri selectedImage = data.getData();
+            InputStream inputStream = null;
+            try
+            {
+                assert selectedImage != null;
+                inputStream= getContentResolver().openInputStream(selectedImage);
+            }
+            catch (FileNotFoundException e)
+            {
+                e.printStackTrace();
+            }
+            BitmapFactory.decodeStream(inputStream);
+            profilePic_Image.setImageURI(selectedImage);
+            }
+        }
+
+   /* @Override
+    protected void onStart() {
+        super.onStart();
+
+    }*/
+
+
 }
